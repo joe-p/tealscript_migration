36276067e36a90729be26a411bc8263667e4ffe2
Joe Polny <joepolny@gmail.com>
2025-07-25 12:24:51 -0400

    convert calc

--- smart_contracts/calculator/contract.algo.ts
+++ smart_contracts/calculator/contract.algo.ts
@@ -1,7 +1,26 @@
-import { Contract } from '@algorandfoundation/algorand-typescript'
+import { Contract, uint64, assert } from '@algorandfoundation/algorand-typescript'
 
 export class Calculator extends Contract {
-  public hello(name: string): string {
-    return `Hello, ${name}`
+  private getSum(a: uint64, b: uint64): uint64 {
+    const result: uint64 = a + b
+    return result
+  }
+
+  private getDifference(a: uint64, b: uint64): uint64 {
+    return a >= b ? a - b : b - a
+  }
+
+  public doMath(a: uint64, b: uint64, operation: string): uint64 {
+    let result: uint64
+
+    if (operation === 'sum') {
+      result = this.getSum(a, b)
+    } else if (operation === 'difference') {
+      result = this.getDifference(a, b)
+    } else {
+      assert(false, 'Invalid operation')
+    }
+
+    return result
   }
 }
