1694dd083fa037f696eb08d1e74bdbe440b7dff6
Joe Polny <joepolny@gmail.com>
2025-08-02 18:20:46 -0400

    wip: refactor reti test

--- __test__/reti.test.ts
+++ __test__/reti.test.ts
@@ -1,91 +1,84 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
 import { AlgorandClient } from '@algorandfoundation/algokit-utils/types/algorand-client'
-import { StakingPoolClient } from '../smart_contracts/artifacts/reti/StakingPoolClient.ts'
-import { ValidatorRegistryClient } from '../smart_contracts/artifacts/reti/ValidatorRegistryClient.ts'
 import { describe, expect, it } from 'vitest'
 import stakingPoolTealScript from './StakingPoolTEALScript.arc56.json'
 import validatorRegistryTealScript from './ValidatorRegistryTEALScript.arc56.json'
+import stakingPoolPuya from '../smart_contracts/artifacts/reti/StakingPool.arc56.json'
+import validatorRegistryPuya from '../smart_contracts/artifacts/reti/ValidatorRegistry.arc56.json'
 import { AppClient } from '@algorandfoundation/algokit-utils/types/app-client'
+import { Arc56Contract } from '@algorandfoundation/algokit-utils/types/app-arc56'
+
+async function testBytecodeComparison(
+  puyaSpec: Arc56Contract,
+  tealScriptSpec: Arc56Contract,
+  puyaDeployParams: Record<string, any>,
+  tealScriptDeployParams: Record<string, any>,
+  contractName: string,
+) {
+  const algorand = AlgorandClient.defaultLocalNet()
+  const defaultSender = await algorand.account.localNetDispenser()
+
+  const puyaClient = new AppClient({
+    appId: 0n,
+    appSpec: puyaSpec,
+    algorand,
+    defaultSender,
+  })
+
+  const puyaResult = await puyaClient.compile({
+    deployTimeParams: puyaDeployParams,
+  })
+
+  const tealScriptClient = new AppClient({
+    appId: 0n,
+    appSpec: tealScriptSpec,
+    algorand,
+    defaultSender,
+  })
+
+  const tealScriptResult = await tealScriptClient.compile({
+    deployTimeParams: tealScriptDeployParams,
+  })
+
+  const tealscriptSize = tealScriptResult.approvalProgram.length
+  const puyaSize = puyaResult.approvalProgram.length
+
+  console.debug(`${contractName} - Puya bytecode size:`, puyaSize)
+  console.debug(`${contractName} - TealScript bytecode size:`, tealscriptSize)
+
+  // Print percentage
+  console.debug(
+    `${contractName} - Puya bytecode is ${((puyaSize / tealscriptSize) * 100).toFixed(2)}% of TealScript bytecode (NOTE: Some TEALScript code has not been converted yet)`,
+  )
+
+  expect(puyaSize).toBeLessThanOrEqual(tealscriptSize)
+}
 
 describe('StakingPoolClient', () => {
   it('bytecode is smaller or equal to TEALScript', async () => {
     const algorand = AlgorandClient.defaultLocalNet()
-    const defaultSender = await algorand.account.localNetDispenser()
 
-    const puyaClient = new StakingPoolClient({
-      appId: 0n,
-      defaultSender,
-      algorand,
-    }).appClient
-
-    const puyaResult = await puyaClient.compile({
-      deployTimeParams: { NFD_REGISTRY_APP_ID: 0, FEE_SINK_ADDR: algorand.account.random().publicKey },
-    })
-
-    const tealScriptClient = new AppClient({
-      appId: 0n,
-      // @ts-expect-error: JSON error
-      appSpec: stakingPoolTealScript,
-      algorand,
-      defaultSender,
-    })
-
-    const tealScriptResult = await tealScriptClient.compile({
-      deployTimeParams: { nfdRegistryAppId: 0, feeSinkAddr: algorand.account.random().publicKey },
-    })
-
-    const tealscriptSize = tealScriptResult.approvalProgram.length
-    const puyaSize = puyaResult.approvalProgram.length
-
-    console.debug('Puya bytecode size:', puyaSize)
-    console.debug('TealScript bytecode size:', tealscriptSize)
-
-    // Print percetnage
-    console.debug(
-      `Puya bytecode is ${((puyaSize / tealscriptSize) * 100).toFixed(2)}% of TealScript bytecode (NOTE: Some TEALScript code has not been converted yet)`,
+    await testBytecodeComparison(
+      // @ts-expect-error: JSON import
+      stakingPoolPuya,
+      stakingPoolTealScript,
+      { NFD_REGISTRY_APP_ID: 0, FEE_SINK_ADDR: algorand.account.random().publicKey },
+      { nfdRegistryAppId: 0, feeSinkAddr: algorand.account.random().publicKey },
+      'StakingPool',
     )
-
-    expect(puyaSize).toBeLessThanOrEqual(tealscriptSize)
   })
 })
 
 describe('ValidatorRegistryClient', () => {
   it('bytecode is smaller or equal to TEALScript', async () => {
-    const algorand = AlgorandClient.defaultLocalNet()
-    const defaultSender = await algorand.account.localNetDispenser()
-
-    const puyaClient = new ValidatorRegistryClient({
-      appId: 0n,
-      defaultSender,
-      algorand,
-    }).appClient
-
-    const puyaResult = await puyaClient.compile({
-      deployTimeParams: { NFD_REGISTRY_APP_ID: 0 },
-    })
-
-    const tealScriptClient = new AppClient({
-      appId: 0n,
-      // @ts-expect-error: JSON error
-      appSpec: validatorRegistryTealScript,
-      algorand,
-      defaultSender,
-    })
-
-    const tealScriptResult = await tealScriptClient.compile({
-      deployTimeParams: { nfdRegistryAppId: 0 },
-    })
-
-    const tealscriptSize = tealScriptResult.approvalProgram.length
-    const puyaSize = puyaResult.approvalProgram.length
-
-    console.debug('Puya bytecode size:', puyaSize)
-    console.debug('TealScript bytecode size:', tealscriptSize)
-
-    // Print percetnage
-    console.debug(
-      `Puya bytecode is ${((puyaSize / tealscriptSize) * 100).toFixed(2)}% of TealScript bytecode (NOTE: Some TEALScript code has not been converted yet)`,
+    await testBytecodeComparison(
+      // @ts-expect-error: JSON import
+      validatorRegistryPuya,
+      validatorRegistryTealScript,
+      { NFD_REGISTRY_APP_ID: 0 },
+      { nfdRegistryAppId: 0 },
+      'ValidatorRegistry',
     )
-
-    expect(puyaSize).toBeLessThanOrEqual(tealscriptSize)
   })
 })
