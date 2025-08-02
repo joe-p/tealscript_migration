3b6ce053a9406905e87ba00cf3a325da7dff192d
Joe Polny <joepolny@gmail.com>
2025-08-02 10:30:45 -0400

    wip: compiled reti

--- smart_contracts/reti/compiled.algo.ts
+++ smart_contracts/reti/compiled.algo.ts
@@ -1,9 +1,12 @@
+import { uint64, Global } from '@algorandfoundation/algorand-typescript'
 import { StakingPool } from './stakingPool.algo'
 import { ValidatorRegistry } from './validatorRegistry.algo'
-import { compileArc4 } from '@algorandfoundation/algorand-typescript/arc4'
+import { compileArc4, Address } from '@algorandfoundation/algorand-typescript/arc4'
 
 export function stakingPool() {
-  return compileArc4(StakingPool)
+  return compileArc4(StakingPool, {
+    templateVars: { NFD_REGISTRY_APP_ID: 0 as uint64, FEE_SINK_ADDR: new Address() },
+  })
 }
 export function registry() {
   return compileArc4(ValidatorRegistry)
